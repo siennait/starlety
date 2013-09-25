3b3f97443ad922bd3abd8b4e6e80bb23c6aa04b1
Luchian Chivoiu <luchianchivoiu@188-27-110-205.rdsnet.ro>
2013-09-25 13:39:51 +0300

    added video

--- FollowPlaces/AuditionsViewController.h
+++ FollowPlaces/AuditionsViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import <MediaPlayer/MediaPlayer.h>
+#import <MobileCoreServices/MobileCoreServices.h>
 
 @interface AuditionsViewController : UIViewController<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
--- FollowPlaces/AuditionsViewController.m
+++ FollowPlaces/AuditionsViewController.m
@@ -36,5 +36,54 @@
 }
 
 - (IBAction)TakeVideo:(id)sender {
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    picker.mediaTypes = [[NSArray alloc] initWithObjects: (NSString *) kUTTypeMovie, nil];
+    
+    [self presentViewController:picker animated:YES completion:NULL];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    
+    self.movieURL = info[UIImagePickerControllerMediaURL];
+    
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+    
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+    
+}
+
+- (void)moviePlayBackDidFinish:(NSNotification *)notification {
+    
+    [[NSNotificationCenter defaultCenter]removeObserver:self name:MPMoviePlayerPlaybackDidFinishNotification object:nil];
+    
+    [self.movieController stop];
+    [self.movieController.view removeFromSuperview];
+    self.movieController = nil;
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    
+    self.movieController = [[MPMoviePlayerController alloc] init];
+    [self.movieController setContentURL:self.movieURL];
+    
+    [self.movieController.view setFrame:CGRectMake( 0, 40, 320, 476)];
+    
+    [self.view addSubview:self.movieController.view];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(moviePlayBackDidFinish:)
+                                                 name:MPMoviePlayerPlaybackDidFinishNotification
+                                               object:self.movieController];
+    
+    [self.movieController play];
+    
 }
 @end
